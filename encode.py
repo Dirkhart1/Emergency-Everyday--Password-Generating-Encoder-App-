d869d332f184164bd45576265333136a3076bde8
Alexis Rabago <107593587+Dirkhart1@users.noreply.github.com>
2025-09-01 08:22:03 +0000

    Add files via upload
    Updated the way the password is encoded to increase the randomness.
    
    Cleaned up the code

--- encode.py
+++ encode.py
@@ -1,14 +1,4 @@
 
-
-#Entry = ""
-#encoded_password = ""
-#password = input("Enter Password to encode: ")
-##pass1 = int(input("Enter num: "))
-#pass2 = int(input("Enter num: "))
-#pass3 = int(input("Enter num: "))
-# i = 0
-
-
 def encode1(letter):
   code = ""
   num = ord(letter)
@@ -32,25 +22,25 @@ def encode3(letter):
 
 def encode(password, pass1, pass2, pass3):
   encoded_password = ""
-  number = (pass1 * pass2 + pass3) % 10
+  number = (pass1 * pass2 + pass3) % 9
 
   for letter in password:
-    # be between 33-126
+    # This loop allows the letter to be converted into ASCII, the code is carefully crafted for each ASCII int being beetwen 33 - 126
 
-    # This prints a special chracter
-    if number % 3 == 0 and number % 2 == 0:
+    # If number is = 0 6 or 3
+    if number % 3 == 0 :
       encoded_password += encode1(letter)
 
-    # This prints a Upper case
+    # If number is = 2 4 or 8 
     elif (number % 2 == 0 and number % 3 != 0):
       encoded_password += encode2(letter)  
 
-    #This prints a lowercase
+    # If number is = 1 3 or 7 
     else:
       encoded_password += encode3(letter)
 
     number += ((pass1 * pass2) + (pass2 * pass3) + (pass3 * pass1))
-    number = number % 10
+    number = number % 9
 
   return encoded_password
 
